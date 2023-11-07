447ee0a6bce24e1f260f1416a76ed27bc70093a1
Journeybongbong <77794756+Journeybongbong@users.noreply.github.com>
2023-11-07 15:31:27 +0900

    Feat: Home feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { HomeModule } from './home/home.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true
-  })],
+  }), HomeModule],
   controllers: [AppController],
   providers: [AppService],
 })
