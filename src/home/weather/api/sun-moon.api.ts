2710a862b60d31c1feceaadabffcedcee9f56456
Journeybongbong <77794756+Journeybongbong@users.noreply.github.com>
2023-11-29 22:15:07 +0900

    Feat: add getMoonAgeByMonth

--- src/home/weather/api/sun-moon.api.ts
+++ src/home/weather/api/sun-moon.api.ts
@@ -1,11 +1,12 @@
 import { Injectable } from "@nestjs/common";
 import axios from 'axios';
 import { format } from 'date-fns';
+import { MoonAgeDto } from "src/common/dto/home/moon-age.dto";
 @Injectable()
 export class SunMoonAPI{
   private readonly SUNMOON_URL = process.env.SUNMOON_URL;
   private readonly SUNMOON_KEY = process.env.SUNMOON_KEY;
-
+  private readonly MOONAGE_URL = process.env.MOONAGE_URL;
 
   async getSunMoon(lat: number, lon: number, date: string){
     const locdate = date.replace(/-/g, "")
@@ -27,6 +28,26 @@ export class SunMoonAPI{
 
   }
 
+  async getMoonAgeByMonth(year: string, mon: string){
+    const url = `${this.MOONAGE_URL}&serviceKey=${this.SUNMOON_KEY}&solYear=${year}&solMonth=${mon}`;
+    console.log(`getMoonAge: ${url}`)
+    try{
+      let response = (await axios.get(url)).data.response.body.items.item;
+      let moonAges: MoonAgeDto[] = [];
+      response.forEach(item => {
+          let moonAge = new MoonAgeDto();
+          moonAge.lunAge = Math.floor(item.lunAge);
+          moonAge.solDay = item.solDay.toString();
+          moonAges.push(moonAge);
+      });
+      return moonAges;
+      
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  }
+
   convertToAMPM(time) {
     if (isNaN(Number(time))) {
       return '--:--';
@@ -34,4 +55,5 @@ export class SunMoonAPI{
     let date = new Date(1970, 0, 1, time.substring(0, 2), time.substring(2));
     return date.toLocaleTimeString('ko-KR', { hour: '2-digit', minute: '2-digit', hour12: true });
   }
+
 }
