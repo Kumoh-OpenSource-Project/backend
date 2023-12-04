c6ba17ab13bf26013ca9efb6cf847380577363b5
Journeybongbong <77794756+Journeybongbong@users.noreply.github.com>
2023-12-04 22:21:31 +0900

    !HOTFIX: in CoordinateTransition

--- src/home/weather/api/coordinate.transition.ts
+++ src/home/weather/api/coordinate.transition.ts
@@ -2,12 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CoordinateTransition {
-    private PI = Math.asin(1.0) * 2.0;
-    private DEGRAD = this.PI / 180.0;
-    private RADDEG = 180.0 / this.PI;
-
+    
     
     lamcproj(lon: number, lat: number) {
+        const PI = Math.asin(1.0) * 2.0;
+        const DEGRAD = PI / 180.0;
+        const RADDEG = 180.0 / PI;
         let first = 0;
         let Re = 6371.00877;
         let grid = 5.0;
@@ -27,31 +27,27 @@ export class CoordinateTransition {
         let x = 0;
         let y = 0;
 
-        if (first === 0) {
-            this.PI = Math.asin(1.0) * 2.0;
-            this.DEGRAD = this.PI / 180.0;
-            this.RADDEG = 180.0 / this.PI;
-
+        // if (first === 0) {
             re = Re / grid;
-            slat1 = slat1 * this.DEGRAD;
-            slat2 = slat2 * this.DEGRAD;
-            olon = olon * this.DEGRAD;
-            olat = olat * this.DEGRAD;
+            slat1 = slat1 * DEGRAD;
+            slat2 = slat2 * DEGRAD;
+            olon = olon * DEGRAD;
+            olat = olat * DEGRAD;
 
-            sn = Math.tan(this.PI * 0.25 + slat2 * 0.5) / Math.tan(this.PI * 0.25 + slat1 * 0.5);
+            sn = Math.tan(PI * 0.25 + slat2 * 0.5) / Math.tan(PI * 0.25 + slat1 * 0.5);
             sn = Math.log(Math.cos(slat1) / Math.cos(slat2)) / Math.log(sn);
-            sf = Math.tan(this.PI * 0.25 + slat1 * 0.5);
+            sf = Math.tan(PI * 0.25 + slat1 * 0.5);
             sf = Math.pow(sf, sn) * Math.cos(slat1) / sn;
-            ro = Math.tan(this.PI * 0.25 + olat * 0.5);
+            ro = Math.tan(PI * 0.25 + olat * 0.5);
             ro = re * sf / Math.pow(ro, sn);
             first = 1;
-        }
+        // }
 
-        ra = Math.tan(this.PI * 0.25 + lat * this.DEGRAD * 0.5);
+        ra = Math.tan(PI * 0.25 + lat * DEGRAD * 0.5);
         ra = re * sf / Math.pow(ra, sn);
-        theta = lon * this.DEGRAD - olon;
-        if (theta > this.PI) theta -= 2.0 * this.PI;
-        if (theta < -this.PI) theta += 2.0 * this.PI;
+        theta = lon * DEGRAD - olon;
+        if (theta > PI) theta -= 2.0 * PI;
+        if (theta < -PI) theta += 2.0 * PI;
         theta *= sn;
         x = ra * Math.sin(theta) + xo;
         y = ro - ra * Math.cos(theta) + yo;
